109cae6a3015b5fe045abdfd7087cf30bfb1074a
star__hoshi <kensuke1751@gmail.com>
2016-02-16 02:10:17 +0900

    add alcatraz reducer

--- client/reducers/rootReducer.ts
+++ client/reducers/rootReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 
 import todos from './todos';
+import alcatraz from './alcatraz';
 
 const rootReducer = combineReducers({
-  todos: todos
+  todos: todos,
+  alcatraz: alcatraz
 });
 
 export { rootReducer };
