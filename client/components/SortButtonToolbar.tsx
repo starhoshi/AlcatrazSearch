975d18ef923a4f497c655a1a67f83eb511f8311d
star__hoshi <kensuke1751@gmail.com>
2016-02-15 23:20:08 +0900

    add order by label.

--- client/components/SortButtonToolbar.tsx
+++ client/components/SortButtonToolbar.tsx
@@ -9,24 +9,27 @@ interface SortButtonToolbarProps {
 class SortButtonToolbar extends React.Component<SortButtonToolbarProps, any> {
   render() {
     return (
-      <Nav bsStyle="pills" activeKey={1}>
-        <NavItem eventKey={1}>
-          <Icon name="star"/>
-          Stars
-        </NavItem>
-        <NavItem eventKey={2}>
-          <Icon name="clock-o"/>
-          Last Commit
-        </NavItem>
-        <NavItem eventKey={3}>
-          <Icon name="eye"/>
-          Watches
-        </NavItem>
-        <NavItem eventKey={4}>
-          <Icon name="code-fork"/>
-          Forks
-        </NavItem>
-      </Nav>
+      <div>
+        <span>ORDER BY: </span>
+        <Nav bsStyle="pills" activeKey={1}>
+          <NavItem eventKey={1}>
+            <Icon name="star"/>
+            Stars
+          </NavItem>
+          <NavItem eventKey={2}>
+            <Icon name="clock-o"/>
+            Last Commit
+          </NavItem>
+          <NavItem eventKey={3}>
+            <Icon name="eye"/>
+            Watches
+          </NavItem>
+          <NavItem eventKey={4}>
+            <Icon name="code-fork"/>
+            Forks
+          </NavItem>
+        </Nav>
+      </div>
     );
   }
 }
