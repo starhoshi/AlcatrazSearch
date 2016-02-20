b9d096ef540ba694ebd5f69d29970230c0ab82b6
star__hoshi <kensuke1751@gmail.com>
2016-02-20 21:13:10 +0900

    refactor main.tsx.

--- client/main.tsx
+++ client/main.tsx
@@ -3,26 +3,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Action } from 'redux-actions';
-import App from './containers/App';
-import { rootReducer } from './reducers/rootReducer';
-import { applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { Store, createStore, applyMiddleware } from 'redux';
 import createLogger = require('redux-logger');
-
-import {
-  Store,
-  compose,
-  createStore,
-  bindActionCreators,
-  combineReducers
-} from 'redux';
-
-import {
-  connect,
-  Provider
-} from 'react-redux';
+import { rootReducer } from './reducers/rootReducer';
+import App from './containers/App';
 
 const initialState = {};
-
 
 const loggerMiddleware = (createLogger as any)();
 const createStoreWithMiddleware = applyMiddleware(loggerMiddleware)(createStore);
