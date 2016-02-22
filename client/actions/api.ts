f3e9d007790a8f48088e4856a2f674ddf04fd7ae
star__hoshi <kensuke1751@gmail.com>
2016-02-22 20:59:47 +0900

    code formatter.

--- client/actions/api.ts
+++ client/actions/api.ts
@@ -12,12 +12,12 @@ const requestAlcatraz = createAction<Api>(
 
 const receiveAlcatraz = createAction<Api>(
   types.RECEIVE_ALCATRAZ,
-  (alcatraz : Alcatraz[]) => <Api>{loading: false, result: alcatraz}
+  (alcatraz:Alcatraz[]) => <Api>{loading: false, result: alcatraz}
 );
 
 // TODO: redux-logger action name is undefined...
 const loadAlcatraz = () => {
-  return (dispatch : Redux.Dispatch) => {
+  return (dispatch:Redux.Dispatch) => {
     dispatch(requestAlcatraz());
     fetch("http://json-schema.org/draft-04/schema")
       .then(response => response.json())
