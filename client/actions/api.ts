86983b3a1692618a361237d7ba7a198b3630265a
Kensuke Hoshikawa <Kensuke.Hoshikawa@so-net.co.jp>
2016-02-22 19:22:48 +0900

    add api model with loading, alcatrazList.

--- client/actions/api.ts
+++ client/actions/api.ts
@@ -1,20 +1,23 @@
 import { createAction } from 'redux-actions';
 import * as types from '../constants/ActionTypes';
 import {Alcatraz} from "../models/alcatraz";
+import {Api} from "../models/api";
 import 'isomorphic-fetch';
 
 
-const requestAlcatraz = createAction<void>(
-  types.FETCH_ALCATRAZ
+const requestAlcatraz = createAction<Api>(
+  types.FETCH_ALCATRAZ,
+  () => <Api>{loading: true, result: ""}
 );
 
-const receiveAlcatraz = createAction<Alcatraz[]>(
+const receiveAlcatraz = createAction<Api>(
   types.RECEIVE_ALCATRAZ,
-  (alcatraz:Alcatraz[]) => alcatraz
+  (alcatraz : Alcatraz[]) => <Api>{loading: false, result: alcatraz}
 );
 
+// TODO: redux-logger action name is undefined...
 const loadAlcatraz = () => {
-  return (dispatch:Redux.Dispatch) => {
+  return (dispatch : Redux.Dispatch) => {
     dispatch(requestAlcatraz());
     fetch("http://json-schema.org/draft-04/schema")
       .then(response => response.json())
---
+++ client/models/api.ts
@@ -0,0 +1,6 @@
+import {Alcatraz} from "../models/alcatraz";
+
+export type Api = {
+  loading: boolean;
+  result: Alcatraz[];
+};
